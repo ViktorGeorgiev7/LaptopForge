af5651b5e38cf13550d39fdd9a6dfdc6a4d371aa
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2023-12-23 21:05:23 +0200

    Added Collection page(still being worked on) sidebar and header.

--- Web/LaptopForge.Web/Controllers/LaptopsController.cs
+++ Web/LaptopForge.Web/Controllers/LaptopsController.cs
@@ -1,16 +1,21 @@
-﻿using LaptopForge.Services.Data.IServices;
+﻿using LaptopForge.Data;
+using LaptopForge.Services.Data.IServices;
 using LaptopForge.Web.ViewModels.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace LaptopForge.Web.Controllers
 {
     public class LaptopsController : Controller
     {
         private readonly ICreateLaptop createLaptop;
-		public LaptopsController(ICreateLaptop createLaptop)
+        private readonly ApplicationDbContext db;
+
+        public LaptopsController(ICreateLaptop createLaptop,ApplicationDbContext db)
         {
             this.createLaptop = createLaptop;
+            this.db = db;
         }
 
         [Authorize]
@@ -20,17 +25,22 @@ namespace LaptopForge.Web.Controllers
         }
 
         [HttpPost]
-		[Authorize]
+        [Authorize]
 		public IActionResult Create(CreateLaptopInputModel model)
         {
             if (!this.ModelState.IsValid)
             {
-                return this.View();
+                return this.View(model);
             }
             this.createLaptop.GetLaptop(model);
             // TODO: redirect to the added laptop`s page
             return this.Redirect("/");
         }
 
+        public IActionResult Collection()
+        {
+            this.ViewBag.LaptopCount = this.db.Laptops.Count();
+            return this.View();
+        }
     }
 }
