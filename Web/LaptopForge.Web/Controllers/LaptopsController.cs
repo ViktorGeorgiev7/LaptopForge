391b3356ac3bb3ac936421a01996440eccab8490
ViktorGeorgiev7 <103097161+ViktorGeorgiev7@users.noreply.github.com>
2023-12-25 17:03:37 +0200

    Fixed displaying laptops and pagination

--- Web/LaptopForge.Web/Controllers/LaptopsController.cs
+++ Web/LaptopForge.Web/Controllers/LaptopsController.cs
@@ -3,6 +3,7 @@ using LaptopForge.Services.Data.IServices;
 using LaptopForge.Web.ViewModels.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace LaptopForge.Web.Controllers
@@ -37,15 +38,22 @@ namespace LaptopForge.Web.Controllers
             return this.Redirect("/");
         }
 
-        public IActionResult Collection()
+        public IActionResult Collection(int currentPage = 1)
         {
-            this.ViewBag.Laptops = this.db.Laptops;
-            this.ViewBag.LaptopCount = this.db.Laptops.Count();
-            this.ViewBag.LaptopFrom0To500 = this.db.Laptops.Where(x => x.Price < 500).Count();
-            this.ViewBag.LaptopFrom500To1000 = this.db.Laptops.Where(x => x.Price >= 500 && x.Price < 1000).Count();
-            this.ViewBag.LaptopFrom1000To1500 = this.db.Laptops.Where(x => x.Price >= 1000 && x.Price < 1500).Count();
-            this.ViewBag.LaptopFrom1500To2000 = this.db.Laptops.Where(x => x.Price >= 1500 && x.Price < 2000).Count();
-            this.ViewBag.LaptopAbove2000 = this.db.Laptops.Where(x => x.Price > 2000).Count();
+            var laptops = this.db.Laptops.AsEnumerable();
+            //-------------------------------------------------------------------------
+            this.ViewBag.LaptopCount = laptops.Count();
+            this.ViewBag.LaptopFrom0To500 = laptops.Where(x => x.Price < 500).Count();
+            this.ViewBag.LaptopFrom500To1000 = laptops.Where(x => x.Price >= 500 && x.Price < 1000).Count();
+            this.ViewBag.LaptopFrom1000To1500 = laptops.Where(x => x.Price >= 1000 && x.Price < 1500).Count();
+            this.ViewBag.LaptopFrom1500To2000 = laptops.Where(x => x.Price >= 1500 && x.Price < 2000).Count();
+            this.ViewBag.LaptopAbove2000 = laptops.Where(x => x.Price > 2000).Count();
+            //-------------------------------------------------------------------------
+            this.ViewBag.PageSize = 25;
+            this.ViewBag.TotalPages = Math.Ceiling(laptops.Count() / 25.0);
+            laptops = laptops.Skip((currentPage - 1) * 25).Take(25);
+            this.ViewBag.Laptops = laptops;
+            this.ViewBag.currentPage = currentPage;
             return this.View();
         }
     }
